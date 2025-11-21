8ca9da4c47da61a20d43030f0c21ab519e412323
DatumCode <ldhymas44@gmail.com>
2025-11-20 19:33:17 -0700

    add load test script

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,200 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+    Imported_HAR: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR',
+    },
+  },
+}
+
+// Scenario: Scenario_1 (executor: ramping-vus)
+
+export function scenario_1() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR (executor: ramping-vus)
+
+export function imported_HAR() {
+  let response
+
+  const vars = {}
+
+  group('Login and order - https://pizza.datumpizza.click/', function () {
+    // Homepage
+    response = http.get('https://pizza.datumpizza.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Wed, 29 Oct 2025 05:22:24 GMT',
+        'if-none-match': '"51c0348f23522d8f7263cc82d10723db"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(10)
+
+    // Login
+    response = http.put(
+      'https://pizza-service.datumpizza.click/api/auth',
+      '{"email":"d@jwt.com","password":"diner"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.datumpizza.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+
+    vars['token1'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(10.3)
+
+    // Get menu
+    response = http.get('https://pizza-service.datumpizza.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.datumpizza.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // Get Franchise
+    response = http.get(
+      'https://pizza-service.datumpizza.click/api/franchise?page=0&limit=20&name=*',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.datumpizza.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(10)
+
+    // Purchase Pizza
+    response = http.post(
+      'https://pizza-service.datumpizza.click/api/order',
+      '{"items":[{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.datumpizza.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    vars['pizzaJwt'] = jsonpath.query(response.json(), '$.jwt')[0]
+    sleep(2.1)
+
+    // Verify Pizza
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt":"${vars['pizzaJwt']}"}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.datumpizza.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+  })
+}
